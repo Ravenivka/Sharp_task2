dadecdc11ede1c98d3221eff0b75e6346cc41f73
Raven <raveniv@ya.ru>
2023-12-27 11:19:54 +0500

    Обработка напильником

--- Program.cs
+++ Program.cs
@@ -27,13 +27,13 @@ static int Quad(float coord_X, float coord_Y)
  static void Main()
         {
             // int n = 161;
-            Console.Write("Требуются координаты числа.\n");
+            Console.Write("Требуются координаты числа.\n Числа вводить через ',' .\n");
             Console.Write("Введите Х: ");
             string userInput1 = Console.ReadLine();
-            float X = Convert.ToInt32(userInput1);
+            float X = float.Parse(userInput1);
             Console.Write("Введите Y: ");
             string userInput2 = Console.ReadLine();
-            float Y = Convert.ToInt32(userInput2);
+            float Y = float.Parse(userInput2);
             if ((X==0) ^ (Y==0) )
             {
                 Console.Write("Нулевые координаты не допускаются.\n");
